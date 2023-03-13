b43bdbe0f84fe7bc6cfbe9e32dab1161aadd1359
William Alonso <williamalonso@outlook.com>
2023-03-13 20:08:32 -0300

    :sparkles: creating update component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./views/home/home.component";
 import { ProductListComponent } from "./views/product-list/product-list.component";
 import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
+import { ProductUpdateComponent } from "./components/product/product-update/product-update.component";
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'list', component: ProductListComponent },
-    { path: 'product/create', component: ProductCreateComponent }
+    { path: 'product/create', component: ProductCreateComponent },
+    { path: 'product/update/:id', component: ProductUpdateComponent }
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 
@@ -42,7 +43,8 @@ registerLocaleData(localePt);
     ProductListComponent,
     ProductCreateComponent,
     ProductReadComponent,
-    TableModelComponent
+    TableModelComponent,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
