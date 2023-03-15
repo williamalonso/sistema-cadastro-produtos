f187f3924ce1a3933b14aed912d55feda852e0f6
William Alonso <williamalonso@outlook.com>
2023-03-15 00:31:17 -0300

    :zap: error handler

--- frontend/src/app/services/product/product.service.ts
+++ frontend/src/app/services/product/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from '@angular/material/snack-bar';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, EMPTY, map, Observable } from 'rxjs';
 import { Product } from '../../interfaces/Product';
 
 @Injectable({ // Essa diretiva Injectable permite injetar esse service em um componente
@@ -19,10 +19,11 @@ export class ProductService {
     private http: HttpClient
   ) { }
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'Fechar', {
       horizontalPosition: this.horizontalPosition,
       verticalPosition: this.verticalPosition,
+      panelClass: isError? ['msg-error'] : ['msg-success'], // panelClass é uma propriedade do snackBar do Angular UI. Se for mensagem de erro, exibe a classe Css "msg-error", se não, exibe "msg-success"
     });
   }
 
@@ -31,26 +32,46 @@ export class ProductService {
     Ela recebe como parâmetro uma variável do tipo "Produto" que declaramos na interface
   */
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product);
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      map( (obj) => obj ),
+      catchError( (e) => this.errorHandler(e) )
+    );
   }
 
   read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseUrl);
+    return this.http.get<Product[]>(this.baseUrl).pipe(
+      map( (obj) => obj ),
+      catchError( (e) => this.errorHandler(e) )
+    );
   }
 
   readById(id: any): Observable<Product> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.get<Product>(url);
+    return this.http.get<Product>(url).pipe(
+      map( (obj) => obj ),
+      catchError( (e) => this.errorHandler(e) )
+    );
   }
 
   update(product: Product): Observable<Product> {
     const url = `${this.baseUrl}/${product.id}`;
-    return this.http.put<Product>(url, product);
+    return this.http.put<Product>(url, product).pipe(
+      map( (obj) => obj ),
+      catchError( (e) => this.errorHandler(e) )
+    );
   }
 
   delete(id: any): Observable<Product> {
     const url = `${this.baseUrl}/${id}`; // url com id de um item específico do Banco
-    return this.http.delete<Product>(url); // procura pela url informada e deleta o item de acordo com o id
+    return this.http.delete<Product>(url).pipe( // procura pela url informada e deleta o item de acordo com o id
+      map( (obj) => obj ),
+      catchError( (e) => this.errorHandler(e) )
+    ); 
+  }
+
+  errorHandler(e: any): Observable<any> {
+    this.showMessage("Houve um erro", true);
+    return EMPTY;
   }
 
 }
